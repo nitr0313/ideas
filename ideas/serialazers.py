c4cfaeb60149bdc490b28aa823e6c48f959e12bb
AndrewK <kor313@ya.ru>
2022-12-11 16:31:42 +0300

    fix serializer for ideas list

--- ideas/serialazers.py
+++ ideas/serialazers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Idea
+from django.contrib.auth.models import User
 
 
-class IdeaSerializer(serializers.HyperlinkedModelSerializer):
+class IdeaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Idea
         fields = ['title', 'description']
