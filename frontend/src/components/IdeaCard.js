19d1821dc13c69373b08dd23bdac09e6847a7f7a
AndrewK <kor313@yandex.ru>
2022-12-29 15:08:13 +0300

    fix right button style

--- frontend/src/components/IdeaCard.js
+++ frontend/src/components/IdeaCard.js
@@ -156,7 +156,7 @@ class IdeaCard extends Component {
                     </div>
                 </div>
                 <div className="d-flex w-100 justify-content-between p-2">
-                    <div className="" onClick={() => {
+                    <div onClick={() => {
                         this.leftButtonClick(idea)
                     }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -165,7 +165,7 @@ class IdeaCard extends Component {
                                   d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                         </svg>
                     </div>
-                    <div className="btn btn-info btn-arrow-right" onClick={() => {
+                    <div onClick={() => {
                         this.rightButtonClick(idea)
                     }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
