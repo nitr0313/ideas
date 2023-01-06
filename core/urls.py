0bdae43dd8965f162e2d28936300a02598f80d20
AndrewK <kor313@ya.ru>
2023-01-06 22:56:23 +0300

    add defender for prod server

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from .api import github_hello
 
@@ -25,3 +26,8 @@ urlpatterns = [
     path('', include('frontend.urls')),
     path('', include('accounts.urls')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        path('admin/defender/', include('defender.urls')),
+    ]
